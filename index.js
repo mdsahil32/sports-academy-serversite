a96e2fd3a246be5fc6c0c88248375f12499d38bc
mdsahil32 <mohammedsahil3211@gmail.com>
2023-06-12 23:54:33 +0600

    edit something

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const classesCollection = client.db('sportsAcademy').collection('classes')
     const usersCollection = client.db('sportsAcademy').collection('users')
